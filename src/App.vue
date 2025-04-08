0d018d65673457ffc87d3fd4c31632b662313acc
Debora Lais <deboralaisnm@gmail.com>
2025-04-07 21:59:15 -0300

    feat: setup basic routing with Home page

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Header :logo="logo_src" :alt="app_name" />
+    <div>
+      <router-view></router-view>
+    </div>
+  <Footer />
+  </div>
+</template>
+<script>
+import Header from './components/header/Header.vue'
+import Footer from './components/footer/Footer.vue'
+
+export default {
+  data: function() {
+    return {
+      logo_src: "./assets/logo.png",
+      app_name: "Search"
+    }
+  },
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
