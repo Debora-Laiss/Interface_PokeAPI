d4a9d0c228b8109a947ede7c88b69407962055ae
Debora Lais <deboralaisnm@gmail.com>
2025-04-09 23:32:24 -0300

    fix: adjust  fix deploy issue

--- src/components/layout/header/Header.vue
+++ src/components/layout/header/Header.vue
@@ -60,7 +60,7 @@
 
 <script setup>
 import { useTheme } from 'vuetify'
-import { useI18n } from '../../../lang/lang'
+import { useI18n } from 'vue-i18n'
 import { ref, onMounted } from 'vue'
 
 const theme = useTheme()
